00a0c1f043ccb549091839a941a3d932fd2d188f
Zvika Ferentz <zvika.ferentz@gmail.com>
2014-06-21 19:48:13 -0400

    switch to setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+
 setup(name='maproxy',
-      version='1.0',
-      py_modules=['maproxy'],
+
+    version = "0.0.6",
+    author = "Zvika Ferentz",
+    author_email = "zvika dot ferentz at gmail",
+    description = ("My first attempt to create a simple and awesome "
+                   "TCP proxy using Tornado"),
+    #long_description=open('README').read(),
+
+
+    py_modules=['src/maproxy'],
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet :: Proxy Servers"]
       )
